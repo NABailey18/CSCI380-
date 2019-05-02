d9205b8117c6378e0b4fe2f039f8cc04013b67eb
jeremyhdreyer <jeremy.dreyer7@gmail.com>
2019-05-02 15:56:41 -0400

    its a start? Maybe?

---
+++ app/src/main/java/com/csci38004/assignment2/ViewCakeAnimationsActivity.java
@@ -0,0 +1,22 @@
+package com.csci38004.assignment2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ViewCakeAnimationsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cake_view_property_animations);
+
+            /* create field for dog cakeview image that needs to be put into cake_view_animations.xml like
+            ImageView dogImageView = findViewById(R.id.dogImageView);
+            AKA WE NEED API FOR THIS
+            */
+    }
+}
---
+++ app/src/main/java/com/csci38004/assignment2/ViewCrepeAnimationsActivity.java
@@ -0,0 +1,21 @@
+package com.csci38004.assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ViewCrepeAnimationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cake_view_property_animations);
+
+            /* create field for dog crepeview image that needs to be put into crepe_view_animations.xml like
+            ImageView dogImageView = findViewById(R.id.dogImageView);
+            AKA WE NEED API FOR THIS
+            */
+    }
+}
